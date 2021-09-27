336af8b25c24da1069f5ab8f7d759248077908d2
eldente <ivo.zubcic@gmail.com>
2021-09-27 23:29:53 +0200

    Added react-bootstrap

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,8 +1,16 @@
 import React from "react";
 import "./App.css";
+import { Button, Card, Container } from "react-bootstrap";
 
 function App() {
-  return <div className="App">Home</div>;
+  return (
+    <Container className="App">
+      <Card.Header>Početna stranica</Card.Header>
+      <Card.Body>
+        <Card.Text>Sadržaj...</Card.Text>
+      </Card.Body>
+    </Container>
+  );
 }
 
 export default App;
